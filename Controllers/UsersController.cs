56e2314497baa07a9f6a5bb37b82613dbd387d97
PhamMinhKha0710 <phamminhkha19091710@gmail.com>
2025-03-10 16:12:56 +0700

    [feat] : repo hien thi kho luu tru dua vao role

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using DoAnWeb.Models;
+using DoAnWeb.Services;
+
+namespace DoAnWeb.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _userService.GetAllUsers();
+            return View(users);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var user = _userService.GetUserById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+    }
+}
